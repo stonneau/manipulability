332d7d67815d746e468eb3043c9b0077ae9a4347
steve tonneau <stevetonneau@hotmail.fr>
2014-06-16 10:08:34 +0200

    correct findxerces script

--- cmake/FindXerces.cmake
+++ cmake/FindXerces.cmake
@@ -1,49 +1,35 @@
-# Base Io build system
-# Written by there.exists.teslos<there.exists.teslos.gmail.com>
+
 #
-# Find ODE Open Dynamics Engine
-FIND_PATH(ODE_INCLUDE_DIR ode/ode.h
-    /usr/include
-    /usr/local/include
+# Find the native Xerces includes and library
+#
+# XERCES_INCLUDE_DIR - where to find dom/dom.hpp, etc.
+# XERCES_LIBRARIES   - List of fully qualified libraries to link against
+# XERCES_FOUND       - Do not attempt to use Xerces if "no" or undefined.
+
+FIND_PATH(XERCES_INCLUDE_DIR dom/dom.hpp
+  /usr/local/include
+  /usr/include
 )
-FIND_PATH(DRAWSTUFF_INCLUDE_DIR drawstuff/drawstuff.h
-    /Users/toniivas/Documents/Sources/ode-0.12/include
+
+# There may be some API changes between Xerces 1.x and 2.x
+# I'm not sure how to deal with that in a .cmake file
+# Perhaps it should be up to the application to figure out the version and
+# API specifics from macros set in the headers?
+
+FIND_LIBRARY(XERCES_LIBRARY
+  NAMES
+    xerces-c_2
+    xerces-c_2D
+    xerces-c_1
+    xerces-c_1D
+  PATHS
+    /usr/local/lib
+    /usr/lib
 )
 
-SET(ODE_NAMES ${ODE_NAMES} ode libode)
-SET(DRAW_NAMES ${DRAW_NAMES} drawstuff libdrawstuff)
-FIND_LIBRARY(ODE_LIBRARY NAMES ${ODE_NAMES} PATH)
-FIND_LIBRARY(DRAWSTUFF_LIBRARY NAMES ${DRAW_NAMES} PATH)
-
-IF(ODE_INCLUDE_DIR)
-    MESSAGE(STATUS "Found ODE include dir: ${ODE_INCLUDE_DIR}")
-ELSE(ODE_INCLUDE_DIR)
-    MESSAGE(STATUS "Couldn't find ODE include dir: ${ODE_INCLUDE_DIR}")
-ENDIF(ODE_INCLUDE_DIR)
-
-IF(ODE_LIBRARY)
-    MESSAGE(STATUS "Found ODE library: ${ODE_LIBRARY}")
-ELSE(ODE_LIBRARY)
-    MESSAGE(STATUS "Couldn't find ODE library: ${ODE_LIBRARY}")
-ENDIF(ODE_LIBRARY)
-
-IF(DRAWSTUFF_LIBRARY)
-    MESSAGE(STATUS "Found DrawStuff library: ${DRAWSTUFF_LIBRARY}")
-ELSE(DRAWSTUFF_LIBRARY)
-    MESSAGE(STATUS "Coulddn't find DrawStuff library: ${DRAWSTUFF_LIBRARY}")
-ENDIF(DRAWSTUFF_LIBRARY)
-
-IF(ODE_INCLUDE_DIR AND ODE_LIBRARY)
-SET(ODE_FOUND TRUE CACHE STRING "Whether ODE was found or not")
-ENDIF(ODE_INCLUDE_DIR AND ODE_LIBRARY)
-
-IF(ODE_FOUND)
-    SET(CMAKE_C_FLAGS "-DdSINGLE")
-IF(NOT ODE_FIND_QUIETLY)
-MESSAGE(STATUS "Found ODE: ${ODE_LIBRARY}")
-ENDIF (NOT ODE_FIND_QUIETLY)
-ELSE(ODE_FOUND)
-IF(ODE_FIND_REQUIRED)
-MESSAGE(FATAL_ERROR "Could not find ODE")
-ENDIF(ODE_FIND_REQUIRED)
-ENDIF(ODE_FOUND)
+IF(XERCES_INCLUDE_DIR)
+  IF(XERCES_LIBRARY)
+    SET( XERCES_LIBRARIES ${XERCES_LIBRARY} )
+    SET( XERCES_FOUND "YES" )
+  ENDIF(XERCES_LIBRARY)
+ENDIF(XERCES_INCLUDE_DIR)
